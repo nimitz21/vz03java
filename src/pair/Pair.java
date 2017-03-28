22a3a61a2c78781c1082d03c4dd7297bd8b3fa85
emonnemo <dika.kusuma23@gmail.com>
2017-03-28 18:22:37 +0700

    Pair

--- src/pair/Pair.java
+++ src/pair/Pair.java
@@ -1,7 +1,23 @@
 package pair;
 
 /**
- * Created by ASUS INDONESIA on 3/28/2017.
+ * Created by Reinaldo on 3/27/2017.
  */
 public class Pair {
+  public int first;
+  public int second;
+
+  public Pair() {
+    first = 0;
+    second = 0;
+  }
+
+  public Pair(int _first, int _second) {
+    first = _first;
+    second = _second;
+  }
+
+  public boolean Equals(Pair pair) {
+    return first == pair.first && second == pair.second;
+  }
 }
