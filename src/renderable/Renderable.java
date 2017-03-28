0ada371f27881c01cee4261a368fa4b342f7cf11
Gisela <giselasupardi@gmail.com>
2017-03-28 19:21:22 +0700

    renderable

---
+++ src/renderable/Renderable.java
@@ -0,0 +1,15 @@
+package renderable;
+
+/**
+ * Interface sebagai representasi sel-sel
+ * dalam matriks pada <code>Zoo</code>.
+ *
+ * @author Gisela Supardi
+ * @version %I%, %G%
+ */
+public interface Renderable {
+  /**
+   * Menampilkan representasi objek pada layar.
+   */
+  void Render();
+}
