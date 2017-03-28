ad636ae7961a83d30f3ed8091b4da4eb42e9e225
emonnemo <dika.kusuma23@gmail.com>
2017-03-28 18:19:29 +0700

    awl awal

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import driver.Driver;
+
+/**
+ * Created by ASUS INDONESIA on 3/28/2017.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Driver d = new Driver();
+  }
+}
---
+++ src/driver/Driver.java
@@ -0,0 +1,7 @@
+package driver;
+
+/**
+ * Created by ASUS INDONESIA on 3/28/2017.
+ */
+public class Driver {
+}
