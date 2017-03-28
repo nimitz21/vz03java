e6041e536d05b82b51a8f3f104abb873c7b65801
Reinaldo Ignatius <rei_iw@yahoo.com>
2017-03-28 23:50:29 +0700

    komentar pair

--- src/pair/Pair.java
+++ src/pair/Pair.java
@@ -1,23 +1,42 @@
+/* File  : Pair.java */
 package pair;
 
 /**
- * Created by Reinaldo on 3/27/2017.
+ * Kelas yang terdiri dari sepasang <code>integer</code>.
+ *
+ * @author Reinaldo Ignatius Wijaya
+ * @version %I%, %G%
  */
 public class Pair {
-  public int first;
-  public int second;
+	public int first;
+	public int second;
 
-  public Pair() {
-    first = 0;
-    second = 0;
-  }
+	/**
+	 * Konstruktor tanpa parameter.
+	 */
+	public Pair() {
+		first = 0;
+		second = 0;
+	}
 
-  public Pair(int _first, int _second) {
-    first = _first;
-    second = _second;
-  }
+	/**
+	 * Konstruktor dengan parameter.
+	 *
+	 * @param _first  <code>integer</code> pertama.
+	 * @param _second <code>integer</code> kedua.
+	 */
+	public Pair(int _first, int _second) {
+		first = _first;
+		second = _second;
+	}
 
-  public boolean Equals(Pair pair) {
-    return first == pair.first && second == pair.second;
-  }
+	/**
+	 * Mengecek kesamaan 2 buah <code>Pair</code>.
+	 *
+	 * @param pair pasangan <code>integer</code> yang akan dicek kesamaannya.
+	 * @return <code>true</code> jika kedua <code>Pair</code> sama, <code>false</code> jika tidak.
+	 */
+	public boolean Equals(Pair pair) {
+		return first == pair.first && second == pair.second;
+	}
 }
